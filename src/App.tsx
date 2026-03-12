8dfdef4f3fadff32c0d02a551819f792a3a67fe5
srchd <sipirichi@gmail.com>
2026-03-12 22:24:56 +0100

    Removed TODO list from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,6 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Board from "./pages/Board"
 
-declare const PeerVeClient: any;
-
-// TODO:
-//  - Remove PeerVeClient declaration from here
-//  - Add a How To Use link to Dashboard, navigating to README on GitHub
-//  - Modify README to correspond for the hosted webapp
-//  - Find a way to save apiKey to an env variable before hosting
-//  - Host it on GitHub pages
-
 export default function App() {
   const { user } = useAuth();
 

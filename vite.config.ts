a98ca402c5cfd3e00032094a7aa4b21fb53970eb
srchd <sipirichi@gmail.com>
2026-04-11 14:32:50 +0200

    optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+  },
   plugins: [
     react(),
     tailwindcss(),
